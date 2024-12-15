6fa0ea9ef8885ff4bdce8d2ec50db7686b917681
BHARANICSE1418 <61092396+BHARANICSE1418@users.noreply.github.com>
2024-12-15 10:50:11 +0530

    Update TestRunner.java

--- cucumberfbopen/src/test/java/stepDefinitions/TestRunner.java
+++ cucumberfbopen/src/test/java/stepDefinitions/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features", 
                  glue= {"stepDefinitions"},
-                 plugin= { "pretty","html:target/html/""json:target/json/file.json""com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
+                 plugin= { "pretty","html:target/html/","json:target/json/file.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, 
                  tags= "@Facebookopen",
                  monochrome= true
                  )
